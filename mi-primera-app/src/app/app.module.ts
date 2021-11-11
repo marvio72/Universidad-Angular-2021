3bf1ad9c5bebf202edbf58b7868a90b0c2412821
marvio72 <mruvalcaba@oversistemas.com>
2021-11-11 17:54:28 -0600

    Creación de un Componente en Angular (con CLI)

--- mi-primera-app/src/app/app.module.ts
+++ mi-primera-app/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PersonasComponent } from './personas/personas.component';
+import { PersonaComponent } from './persona/persona.component';
 
 @NgModule({
-  declarations: [AppComponent, PersonasComponent],
+  declarations: [AppComponent, PersonasComponent, PersonaComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
