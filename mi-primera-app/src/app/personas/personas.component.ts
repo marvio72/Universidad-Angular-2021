56519d0b68a3cd171c6a725241b05fc9a63475c9
marvio72 <mruvalcaba@oversistemas.com>
2021-11-11 19:27:44 -0600

    Instalación de Bootstrap en Angular

--- mi-primera-app/src/app/personas/personas.component.ts
+++ mi-primera-app/src/app/personas/personas.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-personas',
-  template:
-    '<h1>Listado de Personas</h1><app-persona></app-persona><app-persona></app-persona><app-persona></app-persona>',
-  // templateUrl: './personas.component.html',
+  templateUrl: './personas.component.html',
 })
 export class PersonasComponent {}
