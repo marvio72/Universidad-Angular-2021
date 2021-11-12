f63792d8f8c3014fff64c49d23462ed7988bbb30
marvio72 <mruvalcaba@oversistemas.com>
2021-11-11 18:54:52 -0600

    Generación de componentes en Linea

--- mi-primera-app/src/app/personas/personas.component.ts
+++ mi-primera-app/src/app/personas/personas.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-personas',
-  templateUrl: './personas.component.html',
+  template:
+    '<h1>Listado de Personas</h1><app-persona></app-persona><app-persona></app-persona><app-persona></app-persona>',
+  // templateUrl: './personas.component.html',
 })
 export class PersonasComponent {}
