bf781e26f15091eeb4f4ab8467c72b9d1e5f07ed
marvio72 <mruvalcaba@oversistemas.com>
2021-12-04 16:43:03 -0600

    Creación clase Personas

---
+++ listado-personas/src/app/persona.model.ts
@@ -0,0 +1,12 @@
+export class Persona {
+  //   nombre: string = '';
+  //   apellido: string = '';
+
+  //   constructor(nombre: string, apellido: string) {
+  //     (this.nombre = nombre), (this.apellido = apellido);
+  //   }
+
+  // Manera Simplificada
+
+  constructor(public nombre: string, public apellido: string) {}
+}
