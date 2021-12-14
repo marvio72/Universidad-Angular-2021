85d6c9be3531e4f95f2dd5596a495cbd5e49f1b8
marvio72 <mruvalcaba@oversistemas.com>
2021-12-13 19:20:45 -0600

    Modularización Aplicación Calculadora en Angular

--- calculadora-app/src/app/app.module.ts
+++ calculadora-app/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, FormularioComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
